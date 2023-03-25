c40ff4b3b9c3c490a94b90ff83d879e05c72db3c
WangTao <1609832760@qq.com>
2023-03-25 18:09:49 +0800

    java核心基础

---
+++ JavaSenior/day_3_22/src/caom/atTao/java1/MyInterface.java
@@ -0,0 +1,15 @@
+package caom.atTao.java1;
+
+/**
+ * 自定义函数式接口
+ *
+ * @version 1.0
+ * @Author T-WANG
+ * @Date 2023/3/24 20:18
+ */
+@FunctionalInterface
+public interface MyInterface {
+
+    void method1();
+
+}
