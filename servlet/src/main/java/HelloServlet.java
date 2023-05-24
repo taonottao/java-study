55aecf5b92a36612213d265c79c285e063d25a01
WangTao <1609832760@qq.com>
2023-05-24 08:04:27 +0800

    servlet学习

---
+++ servlet/src/main/java/HelloServlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @version 1.0
+ * @Author T-WANG
+ * @Date 2023/5/24 0:06
+ */
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
