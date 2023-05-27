573abf0ac443d7eed335970798329d10c90f8bcb
WangTao <1609832760@qq.com>
2023-05-27 18:03:59 +0800

    servlet学习

--- servlet/src/main/java/HelloServlet.java
+++ servlet/src/main/java/HelloServlet.java
@@ -1,3 +1,5 @@
+import com.sun.deploy.model.DownloadDelegate;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,12 +14,39 @@ import java.io.IOException;
  */
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
+//    @Override
+//    public void init() throws ServletException {
+//        System.out.println("执行 init");
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        System.out.println("执行destroy");
+//    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        super.doGet(req, resp);
         // 这是在服务器的控制台中, 打印了字符串. (服务器看到了, 客户端看不到)
-        System.out.println("hello world");
-        // 这个是给 resp 的 body 中写入 hello world 字符串, 这个内容就会被 HTTP 响应返回给浏览器, 显示在浏览器页面上
-        resp.getWriter().write("hello world");
+//        System.out.println("hello world");
+//        // 这个是给 resp 的 body 中写入 hello world 字符串, 这个内容就会被 HTTP 响应返回给浏览器, 显示在浏览器页面上
+//        resp.getWriter().write("hello world");
+        resp.setContentType("text/html; charset=utf8");
+        resp.getWriter().write("这是doGet");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("doPost");
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("doPut");
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("doDelete");
     }
 }
