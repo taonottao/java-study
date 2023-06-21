90036e865d9d135657ea2aa48b1984d69d7a042f
WangTao <1609832760@qq.com>
2023-06-21 19:31:29 +0800

    SSM 学习

---
+++ JavaEEplus/ssm-demo/src/main/java/com/example/ssmdemo/SsmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsmDemoApplication.class, args);
+    }
+
+}
---
+++ JavaEEplus/ssm-demo/src/main/java/com/example/ssmdemo/entity/Userinfo.java
@@ -0,0 +1,21 @@
+package com.example.ssmdemo.entity;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * @version 1.0
+ * @Author T-WANG
+ * @Date 2023/6/20 23:22
+ */
+@Data
+public class Userinfo {
+    private int id;
+    private String username;
+    private String password;
+    private String photo;
+    private LocalDateTime createtime;
+    private LocalDateTime updatetime;
+    private int state;
+}
