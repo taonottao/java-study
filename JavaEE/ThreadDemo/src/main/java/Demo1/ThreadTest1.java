84cf54765d010f42047dcdeec867c378a26f09b1
WangTao <1609832760@qq.com>
2023-07-20 07:57:14 +0800

    多线程学习

---
+++ JavaEE/ThreadDemo/src/main/java/Demo1/ThreadTest1.java
@@ -0,0 +1,24 @@
+package Demo1;
+
+/**
+ * @version 1.0
+ * @Author T-WANG
+ * @Date 2023/7/19 18:43
+ */
+
+class MyThread extends Thread{
+    @Override
+    public void run() {
+        System.out.println("MyThread线程，启动！");
+    }
+    public void run1(){
+        System.out.println("run1");
+    }
+}
+
+public class ThreadTest1 {
+    public static void main(String[] args) {
+        Thread t = new MyThread();
+        t.start();
+    }
+}
